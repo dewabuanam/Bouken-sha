7cf4cf1e390fa814eac464ac42a3b8a2d6721e51
Dewa Buana Muharmadin <dewa.muharmadin@m2.formulatrix.com>
2022-04-16 16:14:58 +0700

    initial commit

---
+++ Bouken-sha/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Bouken_sha.View;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Globalization;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+namespace Bouken_sha
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App() => this.InitializeComponent();
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            // Prepare the app shell and window content.
+            AppShell shell = Window.Current.Content as AppShell ?? new AppShell();
+            shell.Language = ApplicationLanguages.Languages[0];
+            Window.Current.Content = shell;
+
+            if (shell.AppFrame.Content == null)
+            {
+                // When the navigation stack isn't restored, navigate to the first page
+                // suppressing the initial entrance animation.
+                shell.AppFrame.Navigate(typeof(HomePage), null,
+                    new SuppressNavigationTransitionInfo());
+            }
+
+            Window.Current.Activate();
+        }
+
+    }
+}
---
+++ Bouken-sha/AppShell.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Bouken_sha
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppShell : Page
+    {
+        public AppShell()
+        {
+            this.InitializeComponent();
+
+            Loaded += (sender, args) =>
+            {
+                NavView.SelectedItem = CustomerListMenuItem;
+            };
+
+            // Set up custom title bar.
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            // Set XAML element as a draggable region.
+            Window.Current.SetTitleBar(AppTitleBar);
+
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            titleBar.ButtonForegroundColor = Colors.Black;
+            AppTitle.Text = Windows.ApplicationModel.Package.Current.DisplayName;
+        }
+        public Frame AppFrame => frame;
+
+        private void NavigationView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+        {
+
+        }
+
+        private void NavigationView_BackRequested(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs args)
+        {
+
+        }
+
+        private void OnNavigatingToPage(object sender, Windows.UI.Xaml.Navigation.NavigatingCancelEventArgs e)
+        {
+
+        }
+    }
+}
