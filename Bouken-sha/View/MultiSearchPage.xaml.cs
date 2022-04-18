b0e6cbd48389b2f3f7230e7b23b20cf15e4b9c74
Dewa Buana Muharmadin <dewa.muharmadin@m2.formulatrix.com>
2022-04-18 23:04:19 +0700

    update nav and home page

---
+++ Bouken-sha/View/MultiSearchPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Windows.UI.Xaml.Controls;
+
+namespace Bouken_sha.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MultiSearchPage : Page
+    {
+        public MultiSearchPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Bouken-sha/View/SearchPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using Windows.UI.Xaml.Controls;
+
+namespace Bouken_sha.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SearchPage : Page
+    {
+        public SearchPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
